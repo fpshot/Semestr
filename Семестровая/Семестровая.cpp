fecc1dd9f7d152269fc9ae620fb23ccd226f0076
FpShot <samsam23@mail.ru>
2016-09-14 14:27:02 +0500

    Semestr1.0
    RELEASE!!!
    Добавлен метод Крамера и исправлены ошибки.
    Добавленные функции:
    double* methodKramer(int n, double **matrix)
    double det(int n, double **matrix)
    double** createNewMatrix(int n, double **matrix)

--- Семестровая/Семестровая.cpp
+++ Семестровая/Семестровая.cpp
@@ -10,8 +10,11 @@ using namespace std;
 double* methodGaussa(int n, double **matrix);
 void output(int n, double *matrix);
 void input(int & n, double** & matrix);
-bool checkGaussMatrix(int n, double** & matrix);
+bool checkMatrix(int n, double** & matrix);
 void createInterface();
+double det(int n, double **matrix);
+double* methodKramer(int n, double **matrix);
+double** createNewMatrix(int n, double **matrix);
 
 int main()
 {
@@ -32,7 +35,7 @@ int main()
 			switch (choose)
 			{
 			case 1:
-				if (checkGaussMatrix(n, matrix))
+				if (checkMatrix(n, matrix))
 				{
 					x = methodGaussa(n, matrix);
 					output(n, x);
@@ -40,7 +43,12 @@ int main()
 				else { system("cls"); cout << "Данная матрица не может быть решена методом Гаусса!" << '\n';  }
 				break;
 			case 2:
-				system("cls");
+				if (checkMatrix(n, matrix) && det(n, matrix) != 0)
+				{
+					x = methodKramer(n, matrix);
+					output(n, x);
+				}
+				else { system("cls"); cout << "Данная матрица не может быть решена методом Крамера!" << '\n'; }
 				break;
 			case 3:
 				input(n, matrix);
@@ -115,6 +123,30 @@ void input(int & n, double** & matrix)
 	system("cls");
 }
 
+void output(int n, double *matrix)
+{
+	system("cls");
+	cout << "Ответ:" << '\n';
+	for (int i = 0; i < n; i++)
+	{
+		cout << "x" << i + 1 << " = " << matrix[i] << '\n';
+	}
+}
+
+double** createNewMatrix(int n, double **matrix) //Создание новой матрицы, чтоб изменять ее, а не исходную
+{
+	double **tempMatrix = new double*[n];
+	for (int i = 0; i < n; i++)
+	{
+		tempMatrix[i] = new double[n];
+		for (int j = 0; j <= n; j++)
+		{
+			tempMatrix[i][j] = matrix[i][j];
+		}
+	}
+	return tempMatrix;
+}
+
 void swap(double & a, double & b)
 {
 	double temp;
@@ -123,10 +155,10 @@ void swap(double & a, double & b)
 	a = temp;
 }
 
-bool checkGaussMatrix(int n, double** & matrix)
+bool checkMatrix(int n, double** & matrix)
 {
 	bool flag = false;
-	// Опускаем столбцы содержащие ноль вниз матрицы
+	// Опускаем строки содержащие ноль в диагонале
 	for (int j = 0; j < n; j++)
 	{
 		if (matrix[j][j] == 0)
@@ -168,24 +200,25 @@ bool checkGaussMatrix(int n, double** & matrix)
 
 double* methodGaussa(int n, double **matrix)
 {
+	double **tempMatrix = createNewMatrix(n, matrix);
 	double* x = new double[n];
 	int colTemp = 0;
 	double numberDgnl = 0;
 	for (int i = 0;i<n;i++)
 	{
-		numberDgnl = matrix[i][colTemp]; //Число по диагонали i-ой строки
+		numberDgnl = tempMatrix[i][colTemp]; //Число по диагонали i-ой строки
 		//Делим i-ую строку на numberDgnl
 		for (int j = colTemp; j <= n; j++) 
 		{
-			matrix[i][j] = matrix[i][j] / numberDgnl; 
+			tempMatrix[i][j] = tempMatrix[i][j] / numberDgnl;
 		}
 		//Подводим к нулю числа остальных строк в стобце числа по диагонали i-ой строки
 		for (int j = i + 1; j < n; j++)
 		{
-			numberDgnl = matrix[j][colTemp]; //Число j-ой строки в столбце числа по диагонали i-ой строки
+			numberDgnl = tempMatrix[j][colTemp]; //Число j-ой строки в столбце числа по диагонали i-ой строки
 			for (int h = colTemp; h <= n; h++) 
 			{
-				matrix[j][h] = matrix[j][h] - numberDgnl * matrix[colTemp][h];
+				tempMatrix[j][h] = tempMatrix[j][h] - numberDgnl * tempMatrix[colTemp][h];
 			}
 		}
 		colTemp++;
@@ -194,24 +227,59 @@ double* methodGaussa(int n, double **matrix)
 	{
 		for (int j = 1; j <= n - 1 - i;j++)
 		{
-			matrix[i][n] = matrix[i][n] - matrix[i][i + j] * matrix[i+j][n];
+			tempMatrix[i][n] = tempMatrix[i][n] - tempMatrix[i][i + j] * tempMatrix[i+j][n];
 		}
 	}
 	for (int i = 0; i < n; i++) //Заносим x-сы в отдельный массив
 	{
-		x[i] = matrix[i][n];
+		x[i] = tempMatrix[i][n];
 	}
 	return x;
 }
 
-
-void output(int n, double *matrix)
+double det(int n, double **matrix)
 {
-	system("cls");
-	cout << "Ответ:" << '\n';
+	double **tempMatrix = createNewMatrix(n, matrix);
+	int colDgnl = 0;
+	double numberDgnl = 0;
+	// Обнуляем столбцы ниже главной диагонали
 	for (int i = 0; i < n; i++)
 	{
-		cout << "x" << i + 1 << " = " << matrix[i] << '\n';
+		for (int h = i + 1; h < n; h++)
+		{		
+			numberDgnl = tempMatrix[h][colDgnl];
+			for (int j = colDgnl; j < n; j++)
+			{
+				tempMatrix[h][j] = tempMatrix[h][j] - (tempMatrix[i][j] / tempMatrix[i][colDgnl]) * numberDgnl;
+			}
+		}
+		colDgnl++;
 	}
+	// Находим произведение чисел главной диагонали
+	double mul = 1;
+	for (int i = 0; i < n; i++)
+	{
+		mul = mul * tempMatrix[i][i];
+	}
+	return mul;
 }
+
+double* methodKramer(int n, double **matrix)
+{
+	double* x = new double[n];
+	double** tempMatrix;
+	double det0 = det(n, matrix);
+	// Подставляем y-ки в в каждый столбец, находя определитель
+	for (int j = 0;j < n;j++)
+	{
+		tempMatrix = createNewMatrix(n, matrix);
+		for (int i = 0;i < n;i++)
+		{
+			tempMatrix[i][j] = matrix[i][n];
+		}
+		x[j] = det(n, tempMatrix) / det0; // Делим найденный определитель на главный
+	}
+	return x;
+}
+
 
